9e8c7a9c4b7c07bcf34323faa81cfd556f7a2ef8
Joshua <j.maxwell.alan@gmail.com>
2020-10-09 13:06:32 -0400

    Got column-win worta working

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -3,7 +3,20 @@ export class ColumnWinInspector {
         this.columnToCheck = column;
     }
     inspect() {
-        for () {
+        let consecutiveCounter = 0;
+        let winningPlayerTracker;
+        for (let i = this.columnToCheck.length - 1; i >= 0; i--) {
+            winningPlayerTracker = this.columnToCheck[i];
+            consecutiveCounter++;
+            if (i !== 5) {
+                if (this.columnToCheck[i] === this.columnToCheck[i + 1]) {
+                    consecutiveCounter++
+                } else {
+                    consecutiveCounter = 0;
+                }
+            }
+        }
+        switch (consecutiveCounter) {
 
         }
     }
