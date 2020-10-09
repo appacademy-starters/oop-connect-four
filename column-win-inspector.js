40ede879c4ab495253c2ba73a73618def382d5da
Joshua <j.maxwell.alan@gmail.com>
2020-10-09 14:34:33 -0400

    moved return outside for loop.

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -8,18 +8,19 @@ export class ColumnWinInspector {
         console.log('Column passed in:',column, column.tokens);
         for (let i = 0; i <= 2; i++) {
             console.log('Cheking win logic:',
-                column.tokens[i] === column.tokens[i + 1],
-                column.tokens[i] === column.tokens[i + 2],
-                column.tokens[i] === column.tokens[i + 3],
-                column.tokens[i] !== null);
-            if (column.tokens[i] === column.tokens[i + 1] &&
-                column.tokens[i] === column.tokens[i + 2] &&
-                column.tokens[i] === column.tokens[i + 3] &&
-                column.tokens[i] !== null) {
-                return column.tokens[i];
+            column.getTokenAt(i) === column.getTokenAt(i + 1),
+            column.getTokenAt(i) === column.getTokenAt(i + 2),
+            column.getTokenAt(i) === column.getTokenAt(i + 3),
+            column.getTokenAt(i) !== null);
+            if (
+                column.getTokenAt(i) === column.getTokenAt(i + 1) &&
+                column.getTokenAt(i) === column.getTokenAt(i + 2) &&
+                column.getTokenAt(i) === column.getTokenAt(i + 3) &&
+                column.getTokenAt(i) !== null) {
+                return column.getTokenAt(i);
             }
-            return 0;
         }
+        return 0;
         // console.log('Current winner is possibly:',winningPlayerTracker);
         // if (consecutiveCounter === 4) {
         //     return winningPlayerTracker;
