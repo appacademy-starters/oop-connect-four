0a14ffb47f6328680b7351925047cd87a334a977
Geb <gch910@gmail.com>
2020-12-04 17:32:42 -0600

    test

--- game.js
+++ game.js
@@ -2,9 +2,14 @@ export class Game {
     constructor(player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
+        this.currentPlayer = 1;
     }
     getName() {
         return `${this.player1} vs. ${this.player2}`
     }
-    
+
+    playInColumn() {
+
+    }
+
 }
